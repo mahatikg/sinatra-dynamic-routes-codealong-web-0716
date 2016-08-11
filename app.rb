1f998929ca7b600f9335f8a2aeb3bb0c743ae85a
Mahati Gollamudi <github email address>
2016-08-11 15:41:30 -0400

    Add ways to take inputs

--- app.rb
+++ app.rb
@@ -1,3 +1,4 @@
+
 require_relative 'config/environment'
 
 class App < Sinatra::Base
@@ -11,7 +12,19 @@ class App < Sinatra::Base
   get "/hello/:name" do
     @user_name = params[:name]
     "Hello #{@user_name}!"
+
   end
 
+  get '/goodbye/:name' do
+    @user_name = params[:name]
+    "Goodbye #{@user_name}!"
+
+  end
+
+  get '/multiply/:num1/:num2' do
+    @product = params[:num1].to_i * params[:num2].to_i
+    "#{@product}"
+    
+  end
 
 end
